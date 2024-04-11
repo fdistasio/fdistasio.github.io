59806baa6b51377e2c9206009dc9b5abc2293793
fdistasio <fdistasio02@gmail.com>
2024-04-11 22:14:59 +0200

    mod

--- js/typing_effect.js
+++ js/typing_effect.js
@@ -5,23 +5,15 @@ function startTyping(text, speed = 80) {
     function typeNextChar() {
         if (currentIndex <= text.length) {
             outputElement.textContent = text.substring(0, currentIndex);
-            outputElement.innerHTML += createCursorElement();
+            outputElement.innerHTML += "<span class='cursor'>|</span>";
             currentIndex++;
             setTimeout(typeNextChar, speed);
         } else {
             setInterval(() => {
-                toggleCursorVisibility();
+                const cursor = outputElement.querySelector(".cursor");
+                cursor.style.visibility = cursor.style.visibility === "hidden" ? "visible" : "hidden";
             }, 500);
         }
-    }
-
-    function createCursorElement() {
-        return "<span class='cursor'>|</span>";
-    }
-
-    function toggleCursorVisibility() {
-        const cursor = outputElement.querySelector(".cursor");
-        cursor.style.visibility = cursor.style.visibility === "hidden" ? "visible" : "hidden";
     }
 
     typeNextChar();
